2f4ff6fb8685e0eb3768d52922de11365723e215
Savi Rahiman <savirahiman@Savis-MBP.lan>
2024-05-29 15:23:47 -0400

    uploading final work for module 9 challenge

--- Data Analysis/Pewlett_Hackard_Data_Analysis.sql
+++ Data Analysis/Pewlett_Hackard_Data_Analysis.sql
@@ -1,11 +1,4 @@
-# sql-challenge
-For this project, you’ll design the tables to hold the data from the CSV files, import the CSV files into a SQL database, and then answer questions about the data. That is, you’ll perform data modeling, data engineering, and data analysis, respectively.
-# Steps of this challenge is seperated into four folders. 
-Folder titled 'data' - holds the 6 csv files.
-Folder titled 'Data Modeling' - holds the png file for the ERD (entity relationship diagram) of how the 6 csv files relate to one another.
-Folder titled 'Data Engineering' - is the sql file utilizing the export fuction for PostgreSQL in setting up the ERD. This is used to create the necessary tables with its PK, FK, data types and references 
-Folder titled 'Data Analysis' - is the dql file that leverages the tables created in the Data Engineering step to answer the below questions: 
-# -- Data Analysis 
+-- Data Analysis 
 -- 1. List the employee number, last name, first name, sex, and salary of each employee
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
@@ -63,3 +56,5 @@ SELECT last_name, COUNT(last_name) AS "Count of LN"
 FROM employees
 GROUP BY last_name
 ORDER BY 2 DESC;
+
+
